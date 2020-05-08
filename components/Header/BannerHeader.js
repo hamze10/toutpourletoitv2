b658cdfc70759a562efdaed28aaf18eea53229ea
hamze10 <hamza.mahmoudi@student.vinci.be>
2020-05-08 23:49:18 +0200

    remove webp for banner

--- components/Header/BannerHeader.js
+++ components/Header/BannerHeader.js
@@ -29,13 +29,13 @@ class BannerHeader extends Component {
                 {
                     !loading && !isMobile && 
                         <div className="content">
-                            <img src="/assets/images/banner3.webp" alt="Banniere" />
+                            <img src="/assets/images/banner3.jpg" alt="Banniere" />
                         </div>
                 }
                 {
                     !loading && !!isMobile &&
                         <div className="content">
-                            <img src="/assets/images/banner3_mob.webp" alt="Banniere" />
+                            <img src="/assets/images/banner3_mob.jpg" alt="Banniere" />
                         </div>
                 }
             </React.Fragment>
