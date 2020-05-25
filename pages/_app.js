22ab88c82974e5bc68bba889ac98f8321c6337d2
hamze10 <hamza.mahmoudi@student.vinci.be>
2020-05-25 18:36:32 +0200

    scrollbar designed

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import 'bulma/css/bulma.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import '../styles.css';
 
 export default function MyApp({ Component, pageProps }) {
     return <Component {...pageProps} />
